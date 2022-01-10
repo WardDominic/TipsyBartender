c203cf59472003c621a60e6132750af70e567a9d
Dominic Ward <dominicward@doms-mbp.home>
2022-01-10 19:44:04 +0000

    i love big j

--- app/src/main/java/com/example/thetipsybartender/cocktails.java
+++ app/src/main/java/com/example/thetipsybartender/cocktails.java
@@ -5,67 +5,60 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 
 public class cocktails extends AppCompatActivity {
 
+    SearchView searchView;
+    ListView listView;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cocktails);
+        setContentView(R.layout.activity_main);
 
-        package com.example.test.searchview; //Used from:https://www.javatpoint.com/android-searchview
+        searchView = (SearchView) findViewById(R.id.searchView);
+        listView = (ListView) findViewById(R.id.lv1);
 
-        class MainActivity extends AppCompatActivity {
-            SearchView searchView;
-            ListView listView;
-            ArrayList<String> list;
-            ArrayAdapter<String > adapter;
+        list = new ArrayList<>();
+        list.add("Aperol Spritz");
+        list.add("Aviation");
+        list.add("Bahía Breeze");
+        list.add("City Rickey");
+        list.add("Cuba Libre");
+        list.add("Espresso Martini");
+        list.add("Everybody's Irish");
+        list.add("Mexican Firing Squad");
+        list.add("Mint Julop");
+        list.add("Mojito");
+        list.add("Piña Colada");
+        list.add("Zombie");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
+            public boolean onQueryTextSubmit(String query) {
 
-                searchView = (SearchView) findViewById(R.id.searchView);
-                listView = (ListView) findViewById(R.id.lv1);
-
-                list = new ArrayList<>();
-                list.add("Aperol Spritz");
-                list.add("Aviation");
-                list.add("Bahía Breeze");
-                list.add("City Rickey");
-                list.add("Cuba Libre");
-                list.add("Espresso Martini");
-                list.add("Everybody's Irish");
-                list.add("Mexican Firing Squad");
-                list.add("Mint Julop");
-                list.add("Mojito");
-                list.add("Piña Colada");
-                list.add("Zombie");
-
-                adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-                listView.setAdapter(adapter);
-
-                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                    @Override
-                    public boolean onQueryTextSubmit(String query) {
-
-                        if (list.contains(query)) {
-                            adapter.getFilter().filter(query);
-                        } else {
-                            Toast.makeText(MainActivity.this, "No Match found", Toast.LENGTH_LONG).show();
-                        }
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onQueryTextChange(String newText) {
-                        //    adapter.getFilter().filter(newText);
-                        return false;
-                    }
-                });
+                if (list.contains(query)) {
+                    adapter.getFilter().filter(query);
+                } else {
+                    Toast.makeText(cocktails.this, "No Match found", Toast.LENGTH_LONG).show();
+                }
+                return false;
             }
-        }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //    adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 }
